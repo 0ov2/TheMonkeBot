9865ae5fd1e1b1dcc20dd1f752ab83d734761ffa
0ov2 <liamdrop@gmail.com>
2021-01-29 19:59:53 +0000

    Add files via upload

--- commands/autoAvailability.js
+++ commands/autoAvailability.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'autoAvailability',
-    async execute(client){
-        await client.channels.cache.get("761305181827629076").send("<@&" + "803453345187889174" + ">\n" + 
+    async execute(client, ChanID){
+        await client.channels.cache.get(ChanID).send("<@&" + "796855448678563890" + ">\n" + //
         'A - Monday 19 UTC \n' +
         'B - Tuesday 19 UTC \n' +
         'C - Wednesday 19 UTC \n' +
@@ -16,6 +16,42 @@ module.exports = {
             message.react('🇪'),
             message.react('🇫'),
             message.react('🇬');
+        })
+
+        client.on('messageReactionAdd', async (reaction, user) => {
+
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+            if (user.bot) return;
+
+            if (reaction.message.partial) await reaction.message.fetch();
+            if (reaction.partial) await reaction.fetch();
+
+            console.log(reaction.message.id);
+            var countA = reaction.message.reactions.cache.get('🇦').count;
+            var countB = reaction.message.reactions.cache.get('🇧').count;
+            var countC = reaction.message.reactions.cache.get('🇨').count;
+            var countD = reaction.message.reactions.cache.get('🇩').count;
+            var countE = reaction.message.reactions.cache.get('🇪').count;
+            var countF = reaction.message.reactions.cache.get('🇫').count;
+            var countG = reaction.message.reactions.cache.get('🇬').count;
+
+            
+            if (countA > 2) {
+                await reaction.message.reactions.resolve('🇦').users.remove(user.bot.id);
+            } else if (countB > 2) {
+                await reaction.message.reactions.resolve('🇧').users.remove(user.bot.id);
+            } else if (countC > 2) {
+                await reaction.message.reactions.resolve('🇨').users.remove(user.bot.id);
+            } else if (countD > 2) {
+                await reaction.message.reactions.resolve('🇩').users.remove(user.bot.id);
+            } else if (countE > 2) {
+                await reaction.message.reactions.resolve('🇪').users.remove(user.bot.id);
+            } else if (countF > 2) {
+                await reaction.message.reactions.resolve('🇫').users.remove(user.bot.id);
+            } else if (countG > 2) {
+                await reaction.message.reactions.resolve('🇬').users.remove(user.bot.id);
+            }
         })
     }
 }
--- commands/slowMode.js
+++ commands/slowMode.js
@@ -4,21 +4,17 @@ module.exports = {
     name: 'slowmode',
     execute(message, args) {
         const {channel} = message
-        if (message.member.id === "259466508814516224" || message.member.id === "598203336868495360" || message.member.id === "396839144837349376" || message.member.id === "278724509534519297" || message.member.id === "246687193899204608" || message.member.id === "392933270779854848" || message.member.id === "339237828053565450"){
-
-            let duration = args;
-            if (duration === 'off') {
-                duration = 0
-            }
-    
-            if (isNaN(duration)) {
-                message.reply('Please provide either a number of seconds or the word "off"')
-                return
-            }
-
-            channel.setRateLimitPerUser(parseInt(duration))
-        }else {
-            return;
+        
+        let duration = args;
+        if (duration === 'off') {
+            duration = 0
         }
+
+        if (isNaN(duration)) {
+            message.reply('Please provide either a number of seconds or the word "off"')
+            return
+        }
+
+        channel.setRateLimitPerUser(parseInt(duration))
     }
 }
