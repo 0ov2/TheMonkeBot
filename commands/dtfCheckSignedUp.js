de780cd4095d4cec4a6f6596e3c9063a4c2ce83b
0ov2 <liamdrop@gmail.com>
2021-03-13 19:13:10 +0000

    Add files via upload

--- commands/dtfCheckSignedUp.js
+++ commands/dtfCheckSignedUp.js
@@ -2,23 +2,23 @@ const fs = require('fs');
 
 module.exports = {
     name: 'dtfchecksignedup',
-    async execute(message, chan, client) {
+    async execute(chan, client) {
 
         var users = [];
         var usersNotFound = [];
         var options = {encoding: 'utf-8', flag: 'r'};
 
         fs.readFile('./messageIDs/dtfSignedUpIds.txt', options, async function(err, data) {
-            let dataArray = await data.split('\n');
+            let dataArray = data.split('\n');
 
-            const serverId = client.guilds.cache.get(client.guilds.cache.firstKey());
+            const serverId = await client.guilds.cache.get(client.guilds.cache.firstKey());
 
             for (let i = 0; i < dataArray.length; i++) {
                 if (dataArray[i] !== '' ) {
                     if (serverId.client.users.cache.get(dataArray[i])){
 
                         var user = serverId.client.users.cache.get(dataArray[i])
-                        users.push(user.username);
+                        users.push(user.username + ' ' + user.id);
 
                     } else {
                         usersNotFound.push(dataArray[i].toString());
