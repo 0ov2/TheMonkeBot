bd25609672a3d647dc0622fe04e5b3958bcb3fa3
0ov2 <liamdrop@gmail.com>
2021-05-21 12:57:25 +0100

    Add files via upload

--- commands/opMatchAnnouncement.js
+++ commands/opMatchAnnouncement.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 const spacetime = require('spacetime');
-var d = spacetime.now('Europe/London');
-var s = d.add(7, 'day');
-var newDate = ` Sunday ${s.format('{date-pad}')} ${s.format('month')} at 19 UTC vs DT`;
+
 
 module.exports = {
     name: 'opMatchAnnouncement',
     async execute(client, chanid, opRole){  
                                                
+        var d = spacetime.now('Europe/London');
+        var s = d.add(7, 'day');
+        var newDate = ` Sunday ${s.format('{date-pad}')} ${s.format('month')} at 19 UTC vs DT`;
+        
         client.channels.cache.get(chanid.id).send("<@&" + opRole + "> " + `${newDate}`).then(async function (message){
             await message.react('🦧');
 
