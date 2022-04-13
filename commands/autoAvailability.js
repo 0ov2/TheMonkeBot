1e9e33c55dd77cd54b442c42a094bd07ffeeed8c
liam dropinski <liamdrop@gmail.com>
2022-04-13 16:41:32 +0100

    added dynamic local time for availability

--- commands/autoAvailability.js
+++ commands/autoAvailability.js
@@ -1,14 +1,26 @@
 module.exports = {
     name: 'autoAvailability',
     async execute(client, ChanID, opRole){
+        let epochArray = [];
+        
+        for (let i = 1; i < 8; i++){
+            let date = new Date();
+            date.setDate(date.getDate() + i)
+            date.setHours(19);
+            date.setMinutes(0);
+            date.setSeconds(0);
+            date.setMilliseconds(0);
+            epochArray.push({'epoch': date / 1000, 'day': new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date)});
+        }
+
         await client.channels.cache.get(ChanID.id).send("<@&" + opRole + ">\n" + //
-        'A - Monday 18 UTC \n' +
-        'B - Tuesday 18 UTC \n' +
-        'C - Wednesday 18 UTC \n' +
-        'D - Thursday 18 UTC \n' +
-        'E - Friday 18 UTC \n' +
-        'F - Saturday 18 UTC \n' +
-        'G - Sunday 18 UTC').then(async function (message){
+        `A - ${epochArray[0].day} <t:${epochArray[0].epoch}:t> \n` +
+        `B - ${epochArray[1].day} <t:${epochArray[1].epoch}:t> \n` +
+        `C - ${epochArray[2].day} <t:${epochArray[2].epoch}:t> \n` +
+        `D - ${epochArray[3].day} <t:${epochArray[3].epoch}:t> \n` +
+        `E - ${epochArray[4].day} <t:${epochArray[4].epoch}:t> \n` +
+        `F - ${epochArray[5].day} <t:${epochArray[5].epoch}:t> \n` +
+        `G - ${epochArray[6].day} <t:${epochArray[6].epoch}:t>`).then(async (message) => {
             await message.react('🇦'),
             await message.react('🇧'),
             await message.react('🇨'),
--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const Discord = require('discord.js');
 const fs = require('fs');
+const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION", "USER"]});
 const prefix = '-';
 const spacetime = require('spacetime');
