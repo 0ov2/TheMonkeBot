b0b2bc91349537f9e0a0eb2ace61d33f64e15def
liam dropinski <liamdrop@gmail.com>
2022-04-13 13:56:20 +0100

    remvoed unused files

--- commands/uniqueEmojiDirectMessage.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: 'uniqueemojidirectmessage',
-    async execute(user) {
-        user.send("Your reaction to the DTF did not work! You have already signed up.");
-    }
-}
